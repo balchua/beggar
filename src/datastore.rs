9968260bfea82b6a7097d4c84534a5bcae539f86
balchua <balchua@yahoo.com>
2025-02-23 10:21:49 +0800

    add integration test for datastore using postgresql_embedded

--- src/datastore.rs
+++ src/datastore.rs
@@ -264,3 +264,403 @@ impl fmt::Debug for PostgresDatastore {
         f.debug_struct("PostgresDatastore").finish()
     }
 }
+
+#[cfg(test)]
+mod test_utils {
+
+    use postgresql_archive::configuration::zonky;
+    use postgresql_embedded::{PostgreSQL, Settings, VersionReq};
+
+    use crate::error::Result;
+
+    pub async fn initialize_database() -> Result<(PostgreSQL, Settings)> {
+        let postgres_settings = Settings {
+            releases_url: zonky::URL.to_string(),
+            port: 0,
+            version: VersionReq::parse("=17.2.0").unwrap(),
+            ..Default::default()
+        };
+
+        let mut postgresql = PostgreSQL::new(postgres_settings);
+        postgresql.setup().await?;
+        postgresql.start().await?;
+
+        let database_name = "ps_db";
+
+        postgresql.create_database(database_name).await?;
+
+        let settings = postgresql.settings().clone();
+        // let pg = Arc::new(postgresql);
+
+        Ok((postgresql, settings.clone()))
+    }
+
+    pub async fn cleanup_database(pg: PostgreSQL) {
+        pg.stop().await.expect("failed to stop postgres");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::Ds;
+
+    use super::*;
+    use test_utils::cleanup_database;
+    use test_utils::initialize_database;
+
+    #[tokio::test]
+    async fn test_save_and_get_s3_item_detail() -> Result<()> {
+        let (pg, pg_settings) = initialize_database().await?;
+
+        let s = Settings {
+            datasource: Ds {
+                host: pg_settings.host,
+                port: pg_settings.port,
+                db: "ps_db".to_string(),
+                user: pg_settings.username,
+                password: pg_settings.password,
+                min_connections: 1,
+                max_connections: 2,
+                schema: "public".to_string(),
+                acquire_slow_threshold: 100,
+                test_before_acquire: true,
+            },
+        };
+
+        let datastore = PostgresDatastore::new(&s);
+        datastore.migrate().await?;
+
+        let item = S3ItemDetail {
+            bucket: "test_bucket".to_string(),
+            key: "test_key".to_string(),
+            metadata: "{}".to_string(),
+            internal_info: "{}".to_string(),
+            last_modified: chrono::Utc::now().naive_utc(),
+            e_tag: "test_etag".to_string(),
+            data_location: "test_location".to_string(),
+        };
+
+        datastore.save_s3_item_detail(&item).await?;
+
+        let retrieved_item = datastore
+            .get_s3_item_detail("test_bucket", "test_key")
+            .await?;
+
+        assert!(retrieved_item.is_some());
+        assert_eq!(retrieved_item.unwrap().e_tag, "test_etag".to_string());
+
+        cleanup_database(pg).await;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_get_s3_item_detail_with_filter() -> Result<()> {
+        let (pg, pg_settings) = initialize_database().await?;
+
+        let s = Settings {
+            datasource: Ds {
+                host: pg_settings.host,
+                port: pg_settings.port,
+                db: "ps_db".to_string(),
+                user: pg_settings.username,
+                password: pg_settings.password,
+                min_connections: 1,
+                max_connections: 2,
+                schema: "public".to_string(),
+                acquire_slow_threshold: 100,
+                test_before_acquire: true,
+            },
+        };
+
+        let datastore = PostgresDatastore::new(&s);
+        datastore.migrate().await?;
+
+        let item1 = S3ItemDetail {
+            bucket: "test_bucket".to_string(),
+            key: "test_key_1".to_string(),
+            metadata: "{}".to_string(),
+            internal_info: "{}".to_string(),
+            last_modified: chrono::Utc::now().naive_utc(),
+            e_tag: "test_etag_1".to_string(),
+            data_location: "test_location_1".to_string(),
+        };
+
+        let item2 = S3ItemDetail {
+            bucket: "test_bucket".to_string(),
+            key: "test_key_2".to_string(),
+            metadata: "{}".to_string(),
+            internal_info: "{}".to_string(),
+            last_modified: chrono::Utc::now().naive_utc(),
+            e_tag: "test_etag_2".to_string(),
+            data_location: "test_location_2".to_string(),
+        };
+
+        datastore.save_s3_item_detail(&item1).await?;
+        datastore.save_s3_item_detail(&item2).await?;
+
+        let retrieved_items = datastore
+            .get_s3_item_detail_with_filter("test_bucket", "test_key")
+            .await?;
+
+        assert_eq!(retrieved_items.len(), 2);
+        assert_eq!(retrieved_items[0].e_tag, "test_etag_1".to_string());
+        assert_eq!(retrieved_items[1].e_tag, "test_etag_2".to_string());
+
+        cleanup_database(pg).await;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_get_all_buckets() -> Result<()> {
+        let (pg, pg_settings) = initialize_database().await?;
+
+        let s = Settings {
+            datasource: Ds {
+                host: pg_settings.host,
+                port: pg_settings.port,
+                db: "ps_db".to_string(),
+                user: pg_settings.username,
+                password: pg_settings.password,
+                min_connections: 1,
+                max_connections: 2,
+                schema: "public".to_string(),
+                acquire_slow_threshold: 100,
+                test_before_acquire: true,
+            },
+        };
+
+        let datastore = PostgresDatastore::new(&s);
+        datastore.migrate().await?;
+
+        let item1 = S3ItemDetail {
+            bucket: "test_bucket_1".to_string(),
+            key: "test_key_1".to_string(),
+            metadata: "{}".to_string(),
+            internal_info: "{}".to_string(),
+            last_modified: chrono::Utc::now().naive_utc(),
+            e_tag: "test_etag_1".to_string(),
+            data_location: "test_location_1".to_string(),
+        };
+
+        let item2 = S3ItemDetail {
+            bucket: "test_bucket_2".to_string(),
+            key: "test_key_2".to_string(),
+            metadata: "{}".to_string(),
+            internal_info: "{}".to_string(),
+            last_modified: chrono::Utc::now().naive_utc(),
+            e_tag: "test_etag_2".to_string(),
+            data_location: "test_location_2".to_string(),
+        };
+
+        datastore.save_s3_item_detail(&item1).await?;
+        datastore.save_s3_item_detail(&item2).await?;
+
+        let retrieved_buckets = datastore.get_all_buckets().await?;
+
+        assert_eq!(retrieved_buckets.len(), 2);
+        assert!(retrieved_buckets.contains(&"test_bucket_1".to_string()));
+        assert!(retrieved_buckets.contains(&"test_bucket_2".to_string()));
+
+        cleanup_database(pg).await;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_save_and_get_multipart_upload() -> Result<()> {
+        let (pg, pg_settings) = initialize_database().await?;
+
+        let s = Settings {
+            datasource: Ds {
+                host: pg_settings.host,
+                port: pg_settings.port,
+                db: "ps_db".to_string(),
+                user: pg_settings.username,
+                password: pg_settings.password,
+                min_connections: 1,
+                max_connections: 2,
+                schema: "public".to_string(),
+                acquire_slow_threshold: 100,
+                test_before_acquire: true,
+            },
+        };
+
+        let datastore = PostgresDatastore::new(&s);
+        datastore.migrate().await?;
+
+        let upload = MultipartUpload {
+            upload_id: "test_upload_id".to_string(),
+            bucket: "test_bucket".to_string(),
+            key: "test_key".to_string(),
+            last_modified: chrono::Utc::now().naive_utc(),
+            metadata: "{}".to_string(),
+            access_key: "test_access_key".to_string(),
+        };
+
+        datastore.save_multipart_upload(&upload).await?;
+
+        let retrieved_upload = datastore
+            .get_multipart_upload_by_upload_id("test_upload_id")
+            .await?;
+
+        assert!(retrieved_upload.is_some());
+        assert_eq!(
+            retrieved_upload.unwrap().access_key,
+            "test_access_key".to_string()
+        );
+
+        cleanup_database(pg).await;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_save_and_get_multipart_upload_part() -> Result<()> {
+        let (pg, pg_settings) = initialize_database().await?;
+
+        let s = Settings {
+            datasource: Ds {
+                host: pg_settings.host,
+                port: pg_settings.port,
+                db: "ps_db".to_string(),
+                user: pg_settings.username,
+                password: pg_settings.password,
+                min_connections: 1,
+                max_connections: 2,
+                schema: "public".to_string(),
+                acquire_slow_threshold: 100,
+                test_before_acquire: true,
+            },
+        };
+
+        let datastore = PostgresDatastore::new(&s);
+        datastore.migrate().await?;
+
+        // First, save a MultipartUpload
+        let upload = MultipartUpload {
+            upload_id: "test_upload_id".to_string(),
+            bucket: "test_bucket".to_string(),
+            key: "test_key".to_string(),
+            last_modified: chrono::Utc::now().naive_utc(),
+            metadata: "{}".to_string(),
+            access_key: "test_access_key".to_string(),
+        };
+        datastore.save_multipart_upload(&upload).await?;
+
+        // Then, save the MultipartUploadPart
+        let part = MultipartUploadPart {
+            upload_id: "test_upload_id".to_string(),
+            part_number: 1,
+            last_modified: chrono::Utc::now().naive_utc(),
+            md5: "test_md5".to_string(),
+            data_location: "test_location".to_string(),
+        };
+
+        datastore.save_multipart_upload_part(&part).await?;
+
+        let retrieved_parts = datastore.get_parts_by_upload_id("test_upload_id").await?;
+
+        assert_eq!(retrieved_parts.len(), 1);
+        assert_eq!(retrieved_parts[0].md5, "test_md5".to_string());
+
+        cleanup_database(pg).await;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_get_access_key_by_upload_id() -> Result<()> {
+        let (pg, pg_settings) = initialize_database().await?;
+
+        let s = Settings {
+            datasource: Ds {
+                host: pg_settings.host,
+                port: pg_settings.port,
+                db: "ps_db".to_string(),
+                user: pg_settings.username,
+                password: pg_settings.password,
+                min_connections: 1,
+                max_connections: 2,
+                schema: "public".to_string(),
+                acquire_slow_threshold: 100,
+                test_before_acquire: true,
+            },
+        };
+
+        let datastore = PostgresDatastore::new(&s);
+        datastore.migrate().await?;
+
+        let upload = MultipartUpload {
+            upload_id: "test_upload_id".to_string(),
+            bucket: "test_bucket".to_string(),
+            key: "test_key".to_string(),
+            last_modified: chrono::Utc::now().naive_utc(),
+            metadata: "{}".to_string(),
+            access_key: "test_access_key".to_string(),
+        };
+
+        datastore.save_multipart_upload(&upload).await?;
+
+        let retrieved_access_key = datastore
+            .get_access_key_by_upload_id("test_upload_id")
+            .await?;
+
+        assert!(retrieved_access_key.is_some());
+        assert_eq!(retrieved_access_key.unwrap(), "test_access_key".to_string());
+
+        cleanup_database(pg).await;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_delete_multipart_upload_by_upload_id() -> Result<()> {
+        let (pg, pg_settings) = initialize_database().await?;
+
+        let s = Settings {
+            datasource: Ds {
+                host: pg_settings.host,
+                port: pg_settings.port,
+                db: "ps_db".to_string(),
+                user: pg_settings.username,
+                password: pg_settings.password,
+                min_connections: 1,
+                max_connections: 2,
+                schema: "public".to_string(),
+                acquire_slow_threshold: 100,
+                test_before_acquire: true,
+            },
+        };
+
+        let datastore = PostgresDatastore::new(&s);
+        datastore.migrate().await?;
+
+        let upload = MultipartUpload {
+            upload_id: "test_upload_id".to_string(),
+            bucket: "test_bucket".to_string(),
+            key: "test_key".to_string(),
+            last_modified: chrono::Utc::now().naive_utc(),
+            metadata: "{}".to_string(),
+            access_key: "test_access_key".to_string(),
+        };
+
+        datastore.save_multipart_upload(&upload).await?;
+
+        datastore
+            .delete_multipart_upload_by_upload_id("test_upload_id")
+            .await?;
+
+        let retrieved_upload = datastore
+            .get_multipart_upload_by_upload_id("test_upload_id")
+            .await?;
+
+        assert!(retrieved_upload.is_none());
+
+        cleanup_database(pg).await;
+
+        Ok(())
+    }
+}
